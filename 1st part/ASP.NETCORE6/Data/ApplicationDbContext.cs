373d661a23a4070a19b28d17e489a79d47138d08
sallsabilsabbir <sallsabilsabbir@gmail.com>
2022-07-04 15:49:03 +0600

    TempData

---
+++ 1st part/ASP.NETCORE6/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ASP.NETCORE6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NETCORE6.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Category>Categories { get; set; }
+    }
+}
---
+++ 1st part/ASP.NETCORE6/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NETCORE6.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now; 
+        
+
+    }
+}
